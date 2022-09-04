e0ddd12c055bd94a628cea820069a1abc5a93075
Haibo <18045187335@163.com>
2022-09-04 17:44:01 +0800

    添加工程到git2

---
+++ active/src/main/java/com/attive/controller/QuickStartController.java
@@ -0,0 +1,14 @@
+package com.attive.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QuickStartController {
+    @RequestMapping("/quick")
+   @ResponseBody
+    public String quick(){
+        return "访问成功";
+    }
+}
